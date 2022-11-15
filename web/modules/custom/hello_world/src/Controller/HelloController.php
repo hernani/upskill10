f1154e72dd3441b93d01b1297cb9f5e199aa57f3
Hernani Borges de Freitas <hernaniborgesedefreitas@gmail.com>
2022-11-15 17:23:08 +0000

    Hello world module.

--- web/modules/custom/hello_world/src/Controller/HelloController.php
+++ web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -9,17 +9,17 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class HelloController extends ControllerBase {
 
-  /**
-   * Display the markup.
-   *
-   * @return array
-   *   Return markup array.
-   */
-  public function content() {
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Hello, World!'),
-    ];
-  }
+    /**
+     * Display the markup.
+     *
+     * @return array
+     *   Return markup array.
+     */
+    public function content() {
+        return [
+            '#type' => 'markup',
+            '#markup' => $this->t('Hello, World!'),
+        ];
+    }
 
 }
